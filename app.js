423a9f9b0ad967c389573173618f6f12d8ee1e93
Abishankari <sabishankaris@gmail.com>
2022-03-18 14:20:12 +0530

    Added Js files

---
+++ app.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const path=require('path');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const passport=require('passport');
+const mongoose=require('mongoose');
+const config=require('./config/database');
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected',()=>{
+    console.log("Connected to database");
+})
+
+// mongoose.connection.on('error',  (err)=>{
+//     console.log("Database Connection error: "+err);
+// })
+
+const app=express();
+
+const users=require('./routes/users');
+//const { config } = require('process');
+
+const port=3000;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname,'public'))); 
+
+app.use(bodyParser.json()); 
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+var session = require('express-session');
+app.use(session({ secret: 'SECRET' }));
+
+require('./config/passport')(passport);
+ 
+app.use('/users',users);
+
+app.get('/',(req,res)=>{
+    res.send("Invalid endpoint");
+})
+
+app.listen(port ,()=>{
+    console.log('Server started');
+})
+ 
